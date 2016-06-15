add694c657c84cc88eda744adbdef14f17a6f34c
StayHungryStayFoolish <bonismo@hotmail.com>
2016-06-15 18:42:42 +0800

    commit

--- sql/db_sql.sql
+++ sql/db_sql.sql
@@ -3,7 +3,7 @@ CREATE DATABASE db_convenientbuy;
 
 DROP TABLE IF EXISTS db_convenientbuy.cb_content;
 CREATE TABLE db_convenientbuy.cb_content (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'PK ID',
+  id BIGINT(20) AUTO_INCREMENT PRIMARY KEY COMMENT 'PK ID',
   category_id BIGINT(20) NOT NULL COMMENT '类别 ID',
   title VARCHAR(200) DEFAULT NULL COMMENT '标题',
   sub_title VARCHAR(100) DEFAULT NULL COMMENT '子标题',
@@ -13,9 +13,26 @@ CREATE TABLE db_convenientbuy.cb_content (
   pic2 VARCHAR(400) DEFAULT NULL COMMENT '图片2绝对路径',
   content DATETIME DEFAULT NULL COMMENT '内容',
   created DATETIME DEFAULT NULL COMMENT '创建时间',
-  updated DATETIME DEFAULT NULL COMMENT '更新时间'
+  updated DATETIME DEFAULT NULL COMMENT '更新时间',
+  KEY category_id(category_id),
+  KEY updated(updated)
 ) COMMENT '首页展示图';
 
 
+DROP TABLE IF EXISTS db_convenientbuy.cb_content_category;
+CREATE TABLE db_convenientbuy.cb_content_category (
+  id BIGINT(20) AUTO_INCREMENT PRIMARY KEY COMMENT '',
+  parent_id BIGINT(20) DEFAULT NULL COMMENT '',
+  name VARCHAR(50) DEFAULT NULL COMMENT '',
+  status INT(1) DEFAULT '1' COMMENT '',
+  sort_order INT(4) DEFAULT NULL COMMENT '',
+  is_parent TINYINT(1) DEFAULT '1' COMMENT '',
+  created DATETIME DEFAULT NULL COMMENT '创建时间',
+  updated DATETIME DEFAULT NULL COMMENT '更新时间',
+  KEY parent_id (parent_id,status) USING BTREE,
+  KEY sort_order(sort_order)
+) COMMENT '';
+
+
 SELECT *
-FROM db_convenientbuy.cb_content;
+FROM db_convenientbuy.cb_content);
