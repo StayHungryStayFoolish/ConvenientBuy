25d9687fd261649dc9cffb57362d0163ca610657
StayHungryStayFoolish <bonismo@hotmail.com>
2016-06-18 01:01:40 +0800

    commit

--- sql/db_sql.sql
+++ sql/db_sql.sql
@@ -4,21 +4,30 @@ USE db_convenientbuy;
 
 DROP TABLE IF EXISTS cb_content;
 CREATE TABLE cb_content (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `category_id` bigint(20) NOT NULL COMMENT '内容类目ID',
-  `title` varchar(200) DEFAULT NULL COMMENT '内容标题',
-  `sub_title` varchar(100) DEFAULT NULL COMMENT '子标题',
-  `title_desc` varchar(500) DEFAULT NULL COMMENT '标题描述',
-  `url` varchar(500) DEFAULT NULL COMMENT '链接',
-  `pic` varchar(300) DEFAULT NULL COMMENT '图片绝对路径',
-  `pic2` varchar(300) DEFAULT NULL COMMENT '图片2',
-  `content` text COMMENT '内容',
-  `created` datetime DEFAULT NULL,
-  `updated` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `category_id` (`category_id`),
-  KEY `updated` (`updated`)
-) COMMENT '首页商品内容展示表';
+  id          BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  category_id BIGINT(20) NOT NULL
+  COMMENT '内容类目ID',
+  title       VARCHAR(200)           DEFAULT NULL
+  COMMENT '内容标题',
+  sub_title   VARCHAR(100)           DEFAULT NULL
+  COMMENT '子标题',
+  title_desc  VARCHAR(500)           DEFAULT NULL
+  COMMENT '标题描述',
+  url         VARCHAR(500)           DEFAULT NULL
+  COMMENT '链接',
+  pic         VARCHAR(300)           DEFAULT NULL
+  COMMENT '图片绝对路径',
+  pic2        VARCHAR(300)           DEFAULT NULL
+  COMMENT '图片2',
+  content     TEXT COMMENT '内容',
+  created     DATETIME               DEFAULT NULL
+  COMMENT '创建时间',
+  updated     DATETIME               DEFAULT NULL
+  COMMENT '更新时间',
+  KEY category_id (category_id),
+  KEY updated (updated)
+)
+  COMMENT '首页商品内容展示表';
 
 
 DROP TABLE IF EXISTS db_convenientbuy.cb_content_category;
