46e45eff6f6067667b80d5877be8f1a90da8d370
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-18 22:08:29 +0800

    commit

--- common/src/main/java/com/convenientbuy/common/pojo/EUDataGridResult.java
+++ common/src/main/java/com/convenientbuy/common/pojo/EUDataGridResult.java
@@ -3,8 +3,6 @@ package com.convenientbuy.common.pojo;
 /**
  * Created by bonismo@hotmail.com
  * 下午10:04 on 16/8/18.
- *
- * EasyUI 树形控件节点[ 应用在后台 CMS 页面中 ]
  */
 public class EUDataGridResult {
     private long id;
---
+++ common/src/main/java/com/convenientbuy/common/pojo/EUTreeNode.java
@@ -0,0 +1,10 @@
+package com.convenientbuy.common.pojo;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:08 on 16/8/18.
+ * <p>
+ * EasyUI 树形控件节点[ 应用在后台 CMS 页面中 ]
+ */
+public class EUTreeNode {
+}
