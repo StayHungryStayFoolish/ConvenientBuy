f199be3716d5b049882aa1d944233c7f29e2e05e
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-18 21:55:05 +0800

    commit

---
+++ manager/manager-service/src/main/java/com/convenientbuy/service/ContentCategoryService.java
@@ -0,0 +1,8 @@
+package com.convenientbuy.service;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午9:51 on 16/8/18.
+ */
+public interface ContentCategoryService {
+}
---
+++ manager/manager-service/src/main/java/com/convenientbuy/service/ContentService.java
@@ -0,0 +1,8 @@
+package com.convenientbuy.service;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午9:55 on 16/8/18.
+ */
+public interface ContentService {
+}
