87463a3cdd5c95fff1fd9ce84d44519a7969eb77
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-18 22:14:19 +0800

    commit

--- common/src/main/java/com/convenientbuy/common/pojo/Result.java
+++ common/src/main/java/com/convenientbuy/common/pojo/Result.java
@@ -1,8 +1,19 @@
 package com.convenientbuy.common.pojo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Created by bonismo@hotmail.com
  * 下午10:31 on 16/8/18.
  */
 public class Result {
+
+    // Jackson 对象
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    // 响应状态
+    private Integer status;
+
+    // 响应消息
+    private String msg;
 }
