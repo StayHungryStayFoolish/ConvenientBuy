cd3987462c267a02c0ae77b0f8a134741918cad2
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-18 22:19:20 +0800

    commit

--- common/src/main/java/com/convenientbuy/common/pojo/Result.java
+++ common/src/main/java/com/convenientbuy/common/pojo/Result.java
@@ -16,4 +16,21 @@ public class Result {
 
     // 响应消息
     private String msg;
+
+    // 响应中的数据
+    private Object data;
+
+    public Result() {
+    }
+
+    public Result(Integer status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static Result build(Integer status, String msg, Object data) {
+        return new Result(status, msg, data);
+    }
+
 }
