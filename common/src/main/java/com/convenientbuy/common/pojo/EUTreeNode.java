f3907dff2f22840a4011f1dc10eae79cf2c7194f
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-18 22:09:24 +0800

    commit

--- common/src/main/java/com/convenientbuy/common/pojo/EUTreeNode.java
+++ common/src/main/java/com/convenientbuy/common/pojo/EUTreeNode.java
@@ -7,4 +7,26 @@ package com.convenientbuy.common.pojo;
  * EasyUI 树形控件节点[ 应用在后台 CMS 页面中 ]
  */
 public class EUTreeNode {
+
+    private long id;
+    private String text;
+    private String state;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
 }
