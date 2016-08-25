3439d9e3eeab9a650aaf78974be31fef205e779b
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-25 22:19:20 +0800

    commit

--- common/src/main/java/com/convenientbuy/common/utils/FtpUtil.java
+++ common/src/main/java/com/convenientbuy/common/utils/FtpUtil.java
@@ -15,6 +15,18 @@ import java.io.InputStream;
  */
 public class FtpUtil {
 
+    /**
+     * FTP 上传文件到服务器
+     * @param host url
+     * @param port 端口
+     * @param username 用户名
+     * @param password 密码
+     * @param basePath 基本url
+     * @param filePath 文件路径
+     * @param fileName 文件名字
+     * @param inputStream 输入流
+     * @return true 上传成功 / false 上传失败
+     */
     public static boolean uploadFile(String host, int port, String username, String password, String basePath, String filePath, String fileName, InputStream inputStream) {
         boolean result = false;
         FTPClient ftpClient = new FTPClient();
