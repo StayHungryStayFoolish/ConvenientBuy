d02035ff8cf728775675ba5e77f363671bb6cba0
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-25 23:34:33 +0800

    commit

--- common/src/main/java/com/convenientbuy/common/utils/HttpClientUtil.java
+++ common/src/main/java/com/convenientbuy/common/utils/HttpClientUtil.java
@@ -1,11 +1,14 @@
 package com.convenientbuy.common.utils;
 
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -132,5 +135,25 @@ public class HttpClientUtil {
         return resultStr;
     }
 
-
+    public static String doPostJson(String url, String json) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultStr = "";
+        try {
+            HttpPost httpPost = new HttpPost();
+            StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
+            httpPost.setEntity(entity);
+            response = httpClient.execute(httpPost);
+            resultStr = EntityUtils.toString(response.getEntity(), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultStr;
+    }
 }
