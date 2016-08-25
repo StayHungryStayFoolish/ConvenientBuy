c1ff5a1dd619496f977268a513a33929def7f9c5
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-25 21:59:05 +0800

    commit

---
+++ common/src/main/java/com/convenientbuy/common/utils/FtpUtil.java
@@ -0,0 +1,16 @@
+package com.convenientbuy.common.utils;
+
+import java.io.InputStream;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午9:56 on 16/8/25.
+ *
+ * Ftp 工具
+ */
+public class FtpUtil {
+
+    public static boolean uploadFile(String host, int port, String userName, String password, String basePath, String filePath, String fileName, InputStream inputStream) {
+
+    }
+}
