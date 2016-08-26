97744251be0b9147bc62ff37cd68bdfdf64e783b
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-26 22:48:52 +0800

    commit

--- common/src/main/java/com/convenientbuy/common/utils/IDUtils.java
+++ common/src/main/java/com/convenientbuy/common/utils/IDUtils.java
@@ -5,9 +5,16 @@ import java.util.Random;
 /**
  * Created by bonismo@hotmail.com
  * 下午10:37 on 16/8/26.
+ * <p>
+ * ID 工具类
  */
 public class IDUtils {
 
+    /**
+     * 生成图片名字
+     *
+     * @return
+     */
     public static String genImageName() {
         long millis = System.currentTimeMillis();
         Random random = new Random();
@@ -16,9 +23,19 @@ public class IDUtils {
         return string;
     }
 
-    public static void main(String[] args) {
-        System.out.println(System.nanoTime());
-        System.out.println(System.currentTimeMillis());
-
+    /**
+     * 生成商品 ID
+     *
+     * @return
+     */
+    public static long getItemId() {
+        long millis = System.currentTimeMillis();
+        Random random = new Random();
+        int end = random.nextInt(99);
+        String string = millis + String.format("%02d", end);
+        long id = new Long(string);
+        return id;
     }
+
+
 }
