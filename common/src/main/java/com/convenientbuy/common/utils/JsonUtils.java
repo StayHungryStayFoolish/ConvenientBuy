b0a5885e2102cb912cd04051c424178f1b8eb3d6
StayHungryStayFoolish <bonismo@hotmail.com>
2016-08-26 22:57:39 +0800

    commit

---
+++ common/src/main/java/com/convenientbuy/common/utils/JsonUtils.java
@@ -0,0 +1,53 @@
+package com.convenientbuy.common.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:49 on 16/8/26.
+ * <p>
+ * Json 工具类
+ * 转换形式:
+ * POJO <---> JSON <--> List
+ */
+public class JsonUtils {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    /**
+     * Object -> JSON
+     * @param data Object 对象
+     * @return
+     */
+    public static String objectToJSON(Object data) {
+        try {
+            String string = MAPPER.writeValueAsString(data);
+            return string;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * JSON -> Class
+     * @param jsonData
+     * @param beanType
+     * @param <T>
+     * @return
+     */
+    public static <T> T jsonToPOJO(String jsonData, Class<T> beanType) {
+        try {
+            T t = MAPPER.readValue(jsonData, beanType);
+            return t;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
