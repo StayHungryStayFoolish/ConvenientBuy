8e244dd7b003e67cd7d7fdcb46ba44e816392547
StayHungryStayFoolish <bonismo@hotmail.com>
2016-09-11 22:49:15 +0800

    commit

---
+++ manager/manager-service/src/main/java/com/convenientbuy/service/ItemService.java
@@ -0,0 +1,12 @@
+package com.convenientbuy.service;
+
+import com.convenientbuy.pojo.CbItem;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:48 on 16/9/11.
+ */
+public interface ItemService {
+
+    CbItem getItemById(long itemId);
+}
