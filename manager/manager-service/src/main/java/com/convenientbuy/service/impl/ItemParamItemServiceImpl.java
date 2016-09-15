27c1df9e47598b70e2992a9a8fc3c92fa706d538
StayHungryStayFoolish <bonismo@hotmail.com>
2016-09-15 23:28:11 +0800

    commit

--- manager/manager-service/src/main/java/com/convenientbuy/service/impl/ItemParamItemServiceImpl.java
+++ manager/manager-service/src/main/java/com/convenientbuy/service/impl/ItemParamItemServiceImpl.java
@@ -1,7 +1,16 @@
 package com.convenientbuy.service.impl;
 
+import com.convenientbuy.common.utils.JsonUtils;
+import com.convenientbuy.mapper.CbItemParamItemMapper;
+import com.convenientbuy.pojo.CbItemParamItem;
+import com.convenientbuy.pojo.CbItemParamItemExample;
 import com.convenientbuy.service.ItemParamItemService;
+import com.convenientbuy.pojo.CbItemParamItemExample.Criteria;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by bonismo@hotmail.com
@@ -9,8 +18,45 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ItemParamItemServiceImpl implements ItemParamItemService {
+
+    @Autowired
+    private CbItemParamItemMapper mapper;
+
     @Override
     public String getItemParamByItemId(Long itemId) {
-        return null;
+        //根据商品id查询规格参数
+        CbItemParamItemExample example = new CbItemParamItemExample();
+        Criteria criteria = example.createCriteria();
+        criteria.andItemIdEqualTo(itemId);
+        //执行查询
+        List<CbItemParamItem> list = mapper.selectByExampleWithBLOBs(example);
+        if (list == null || list.size() == 0) {
+            return "";
+        }
+        //取规格参数信息
+        CbItemParamItem itemParamItem = list.get(0);
+        String paramData = itemParamItem.getParamData();
+        //生成html
+        // 把规格参数json数据转换成java对象
+        List<Map> jsonList = JsonUtils.jsonToList(paramData, Map.class);
+        StringBuffer sb = new StringBuffer();
+        sb.append("<table cellpadding=\"0\" cellspacing=\"1\" width=\"100%\" border=\"1\" class=\"Ptable\">\n");
+        sb.append("    <tbody>\n");
+        for (Map m1 : jsonList) {
+            sb.append("        <tr>\n");
+            sb.append("            <th class=\"tdTitle\" colspan=\"2\">" + m1.get("group") + "</th>\n");
+            sb.append("        </tr>\n");
+            List<Map> list2 = (List<Map>) m1.get("params");
+            for (Map m2 : list2) {
+                sb.append("        <tr>\n");
+                sb.append("            <td class=\"tdTitle\">" + m2.get("k") + "</td>\n");
+                sb.append("            <td>" + m2.get("v") + "</td>\n");
+                sb.append("        </tr>\n");
+            }
+        }
+        sb.append("    </tbody>\n");
+        sb.append("</table>");
+        return sb.toString();
     }
 }
+
