ffba6f1a774ac2105734ca2c0c13c3653ca497e4
StayHungryStayFoolish <bonismo@hotmail.com>
2016-10-14 22:49:27 +0800

    commit

--- manager/manager-web/src/main/java/com/convenientbuy/controller/ContentCategoryController.java
+++ manager/manager-web/src/main/java/com/convenientbuy/controller/ContentCategoryController.java
@@ -1,8 +1,30 @@
 package com.convenientbuy.controller;
 
+import com.convenientbuy.common.pojo.EUTreeNode;
+import com.convenientbuy.service.ContentCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 /**
  * Created by bonismo@hotmail.com
  * 下午10:22 on 16/10/14.
  */
+@Controller
+@RequestMapping("/content/category")
 public class ContentCategoryController {
+
+    @Autowired
+    private ContentCategoryService service;
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<EUTreeNode> getContentCatList(@RequestParam(value = "id", defaultValue = "0") Long parentId) {
+        List<EUTreeNode> list = service.getCategoryList(parentId);
+        return list;
+    }
 }
