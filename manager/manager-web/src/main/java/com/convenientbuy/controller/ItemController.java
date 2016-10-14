c354035f3d9b8550f7e7eb23d01d88ab8db783a2
StayHungryStayFoolish <bonismo@hotmail.com>
2016-10-14 22:22:15 +0800

    commit

--- manager/manager-web/src/main/java/com/convenientbuy/controller/ItemController.java
+++ manager/manager-web/src/main/java/com/convenientbuy/controller/ItemController.java
@@ -1,11 +1,14 @@
 package com.convenientbuy.controller;
 
+import com.convenientbuy.common.pojo.EUDataGridResult;
+import com.convenientbuy.common.pojo.Result;
 import com.convenientbuy.pojo.CbItem;
 import com.convenientbuy.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -18,10 +21,43 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    /**
+     * 根据商品 ID 查询数据
+     * @param itemId
+     * @return
+     */
     @RequestMapping("/item/{itemId}")
     @ResponseBody
     public CbItem getItemById(@PathVariable long itemId) {
         CbItem cbItem = itemService.getItemById(itemId);
         return cbItem;
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param rows
+     * @return
+     */
+    @RequestMapping("/item/list")
+    @ResponseBody
+    public EUDataGridResult getItemList(Integer page, Integer rows) {
+        EUDataGridResult result = itemService.getItemList(page, rows);
+        return result;
+    }
+
+    /**
+     * 添加商品基本信息/描述/规格参数
+     * @param item 商品
+     * @param desc 商品描述
+     * @param itemParams 规格参数
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value="/item/save", method= RequestMethod.POST)
+    @ResponseBody
+    private Result createItem(CbItem item, String desc, String itemParams) throws Exception {
+        Result result = itemService.createItem(item, desc, itemParams);
+        return result;
     }
 }
