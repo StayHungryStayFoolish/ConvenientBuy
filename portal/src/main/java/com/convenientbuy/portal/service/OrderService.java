5973458976975a4d0648fa132a683a8077daa8ac
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-14 21:53:32 +0800

    commit

--- portal/src/main/java/com/convenientbuy/portal/service/OrderService.java
+++ portal/src/main/java/com/convenientbuy/portal/service/OrderService.java
@@ -1,8 +1,18 @@
 package com.convenientbuy.portal.service;
 
+import com.convenientbuy.portal.pojo.Order;
+
 /**
  * Created by bonismo@hotmail.com
  * 下午9:49 on 16/11/14.
  */
 public interface OrderService {
+
+    /**
+     * 创建订单
+     *
+     * @param order
+     * @return
+     */
+    String createOrder(Order order);
 }
