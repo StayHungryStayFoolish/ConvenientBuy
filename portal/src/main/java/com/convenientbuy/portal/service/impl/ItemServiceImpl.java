537995a450c1b7ebd110995c972c673f99081980
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-14 22:44:17 +0800

    commit

---
+++ portal/src/main/java/com/convenientbuy/portal/service/impl/ItemServiceImpl.java
@@ -0,0 +1,64 @@
+package com.convenientbuy.portal.service.impl;
+
+import com.convenientbuy.common.pojo.Result;
+import com.convenientbuy.common.utils.HttpClientUtil;
+import com.convenientbuy.portal.pojo.ItemInfo;
+import com.convenientbuy.portal.service.ItemService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:38 on 16/11/14.
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    // Rest 服务层基础 URL
+    @Value("${REST_BASE_URL}")
+    private String REST_BASE_URL;
+    // 商品基本信息 URL
+    @Value("${ITEM_INFO_URL}")
+    private String ITEM_INFO_URL;
+    // 商品描述 URL
+    @Value("${ITEM_DESC_URL}")
+    private String ITEM_DESC_URL;
+    @Value("${ITEM_PARAM_URL}")
+    // 商品规格参数 URL
+    private String ITEM_PARAM_URL;
+
+    /**
+     * 根据商品 ID 查询基本信息
+     *
+     * @param itemId
+     * @return
+     */
+    @Override
+    public ItemInfo getItemById(Long itemId) {
+        try {
+            String json = HttpClientUtil.doGet(REST_BASE_URL + ITEM_INFO_URL + itemId);
+            if (!StringUtils.isBlank(json)) {
+                Result result = Result.formatToPojo(json, ItemInfo.class);
+                if (result.getStatus() == 200) {
+                    ItemInfo info = (ItemInfo) result.getData();
+                    return info;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public String getItemDescById(Long itemId) {
+
+        return null;
+    }
+
+    @Override
+    public String getItemParam(Long itemId) {
+        return null;
+    }
+}
