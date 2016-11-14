d60e009738647c924ffe1b22047cdf61bf609542
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-14 22:58:14 +0800

    commit

--- portal/src/main/java/com/convenientbuy/portal/service/impl/ItemServiceImpl.java
+++ portal/src/main/java/com/convenientbuy/portal/service/impl/ItemServiceImpl.java
@@ -2,12 +2,17 @@ package com.convenientbuy.portal.service.impl;
 
 import com.convenientbuy.common.pojo.Result;
 import com.convenientbuy.common.utils.HttpClientUtil;
+import com.convenientbuy.common.utils.JsonUtils;
 import com.convenientbuy.pojo.CbItemDesc;
+import com.convenientbuy.pojo.CbItemParamItem;
 import com.convenientbuy.portal.pojo.ItemInfo;
 import com.convenientbuy.portal.service.ItemService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by bonismo@hotmail.com
@@ -76,6 +81,37 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public String getItemParam(Long itemId) {
-        return null;
+        try {
+            String json = HttpClientUtil.doGet(REST_BASE_URL + ITEM_PARAM_URL + itemId);
+            Result result = Result.formatToPojo(json, CbItemParamItem.class);
+            if (result.getStatus() == 200) {
+                CbItemParamItem itemParamItem = (CbItemParamItem) result.getData();
+                String paramData = itemParamItem.getParamData();
+                // 将规格参数数据转换成对象
+                List<Map> jsonList = JsonUtils.jsonToList(paramData, Map.class);
+                StringBuffer buffer = new StringBuffer();
+                buffer.append("<table cellpadding=\"0\" cellspacing=\"1\" width=\"100%\" border=\"0\" class=\"Ptable\">\n");
+                buffer.append("    <tbody>\n");
+                for (Map m1 : jsonList) {
+                    buffer.append("        <tr>\n");
+                    buffer.append("            <th class=\"tdTitle\" colspan=\"2\">" + m1.get("group") + "</th>\n");
+                    buffer.append("        </tr>\n");
+                    List<Map> list2 = (List<Map>) m1.get("params");
+                    for (Map m2 : list2) {
+                        buffer.append("        <tr>\n");
+                        buffer.append("            <td class=\"tdTitle\">" + m2.get("k") + "</td>\n");
+                        buffer.append("            <td>" + m2.get("v") + "</td>\n");
+                        buffer.append("        </tr>\n");
+                    }
+                }
+                buffer.append("    </tbody>\n");
+                buffer.append("</table>");
+                //返回html片段
+                return buffer.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
