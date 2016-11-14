ebe57e6f6e27a2bd029417774ebcaa10b6238330
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-14 22:26:04 +0800

    commit

--- portal/src/main/java/com/convenientbuy/portal/service/impl/CartServiceImpl.java
+++ portal/src/main/java/com/convenientbuy/portal/service/impl/CartServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.nio.channels.NonWritableChannelException;
 import java.util.ArrayList;
 import java.util.List;
 
---
+++ portal/src/main/java/com/convenientbuy/portal/service/impl/ContentServiceImpl.java
@@ -0,0 +1,16 @@
+package com.convenientbuy.portal.service.impl;
+
+import com.convenientbuy.portal.service.ContentService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:25 on 16/11/14.
+ */
+@Service
+public class ContentServiceImpl implements ContentService {
+    @Override
+    public String getContenList() {
+        return null;
+    }
+}
