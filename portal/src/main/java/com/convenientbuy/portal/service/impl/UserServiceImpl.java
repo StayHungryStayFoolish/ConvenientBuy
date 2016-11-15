62b5025d72487e2d919815d985060f628e712ade
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-16 00:01:39 +0800

    commit

--- portal/src/main/java/com/convenientbuy/portal/service/impl/UserServiceImpl.java
+++ portal/src/main/java/com/convenientbuy/portal/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.convenientbuy.portal.service.impl;
 
+import com.convenientbuy.common.pojo.Result;
+import com.convenientbuy.common.utils.HttpClientUtil;
 import com.convenientbuy.pojo.CbUser;
 import com.convenientbuy.portal.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * Created by bonismo@hotmail.com
@@ -9,9 +12,34 @@ import com.convenientbuy.portal.service.UserService;
  */
 public class UserServiceImpl implements UserService {
 
+    // 单点登录系统基础 URL
+    @Value("${SSO_BASE_URL}")
+    public String SSO_BASE_URL;
+    // 单点登录系统域名
+    @Value("${SSO_DOMAIN_BASE_USRL}")
+    public String SSO_DOMAIN_BASE_USRL;
+    // 根据用户信息获取 Token URL
+    @Value("${SSO_USER_TOKEN}")
+    private String SSO_USER_TOKEN;
 
+    /**
+     * 通过 Token 获取用户信息
+     *
+     * @param token
+     * @return
+     */
     @Override
     public CbUser getUserByToken(String token) {
+        try {
+            String json = HttpClientUtil.doGet(SSO_BASE_URL + SSO_USER_TOKEN + token);
+            Result result = Result.formatToPojo(json, CbUser.class);
+            if (result.getStatus() == 200) {
+                CbUser user = (CbUser) result.getData();
+                return user;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
