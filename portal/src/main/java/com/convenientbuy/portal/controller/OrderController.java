3a0b55964c76fea2b36d9b5a6f64ab0496a3b6e9
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-22 23:00:06 +0800

    commit

--- portal/src/main/java/com/convenientbuy/portal/controller/OrderController.java
+++ portal/src/main/java/com/convenientbuy/portal/controller/OrderController.java
@@ -1,9 +1,16 @@
 package com.convenientbuy.portal.controller;
 
+import com.convenientbuy.portal.pojo.CartItem;
 import com.convenientbuy.portal.service.CartService;
 import com.convenientbuy.portal.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * Created by bonismo@hotmail.com
@@ -19,6 +26,22 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    /**
+     * 展示购物车商品列表
+     *
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
+    @RequestMapping("/order-cart")
+    public String showOrderCart(HttpServletRequest request, HttpServletResponse response, Model model) {
+        // 查询购物车商品列表
+        List<CartItem> list = cartService.getCartItemList(request, response);
+        model.addAttribute("cartList", list);
+        return "order-cart";
+    }
 
 
 }
