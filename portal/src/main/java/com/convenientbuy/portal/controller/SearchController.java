c8f60f71bb4aea5ec99412f2201906ad6b9b3b4f
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-22 23:17:28 +0800

    commit

--- portal/src/main/java/com/convenientbuy/portal/controller/SearchController.java
+++ portal/src/main/java/com/convenientbuy/portal/controller/SearchController.java
@@ -1,8 +1,16 @@
 package com.convenientbuy.portal.controller;
 
+import com.convenientbuy.portal.pojo.SearchResult;
 import com.convenientbuy.portal.service.SearchService;
+import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.UnsupportedEncodingException;
+import java.net.Inet4Address;
 
 /**
  * Created by bonismo@hotmail.com
@@ -13,4 +21,22 @@ public class SearchController {
 
     @Autowired
     private SearchService service;
+
+    @RequestMapping("/search")
+    public String search(@RequestParam("q") String queryString, @RequestParam(defaultValue = "1") Integer page, Model model) {
+        if (null != queryString) {
+            try {
+                // 浏览器西欧编码 ISO8859-1
+                queryString = new String(queryString.getBytes("iso8859-1"), "utf-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        SearchResult searchResult = service.search(queryString, page);
+        model.addAttribute("query", queryString);
+        model.addAttribute("totalPages", searchResult.getPageCount());
+        model.addAttribute("itemList", searchResult.getItemList());
+        model.addAttribute("page", page);
+        return "search";
+    }
 }
