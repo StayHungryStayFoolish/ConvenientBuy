a0e474ab9c17837f42753f7329a48d4ca503f787
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-29 22:50:38 +0800

    commit

---
+++ rest/src/main/java/com/convenientbuy/rest/pojo/CatResult.java
@@ -0,0 +1,20 @@
+package com.convenientbuy.rest.pojo;
+
+import java.util.List;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:50 on 16/11/29.
+ */
+public class CatResult {
+
+    private List<?> data;
+
+    public List<?> getData() {
+        return data;
+    }
+
+    public void setData(List<?> data) {
+        this.data = data;
+    }
+}
