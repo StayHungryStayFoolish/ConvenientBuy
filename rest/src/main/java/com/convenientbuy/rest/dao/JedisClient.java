e1032bf305b7d4369dba99c2acf1a604fa6e7bca
StayHungryStayFoolish <bonismo@hotmail.com>
2016-11-29 22:59:03 +0800

    commit

--- rest/src/main/java/com/convenientbuy/rest/dao/JedisClient.java
+++ rest/src/main/java/com/convenientbuy/rest/dao/JedisClient.java
@@ -6,4 +6,26 @@ package com.convenientbuy.rest.dao;
  */
 public interface JedisClient {
 
+    String get(String key);
+
+    String set(String key, String value);
+
+    String hget(String hkey, String key);
+
+    long hset(String hkey, String key, String value);
+
+    long incr(String key);
+
+    // 设置有效时间
+    long expire(String key, int second);
+
+    // 失效时间
+    long ttl(String key);
+
+    //当数据库发生改变时，清空缓存，以便更新redis数据
+    long del(String key);
+
+    //当数据库发生改变时，清空缓存，以便更新redis数据
+    long hdel(String hkey, String key);
+
 }
--- rest/src/main/java/com/convenientbuy/rest/service/impl/ContentServiceImpl.java
+++ rest/src/main/java/com/convenientbuy/rest/service/impl/ContentServiceImpl.java
@@ -1,7 +1,11 @@
 package com.convenientbuy.rest.service.impl;
 
+import com.convenientbuy.mapper.CbContentMapper;
 import com.convenientbuy.pojo.CbContent;
+import com.convenientbuy.rest.dao.JedisClient;
 import com.convenientbuy.rest.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +17,22 @@ import java.util.List;
 @Service
 public class ContentServiceImpl implements ContentService {
 
+    @Autowired
+    private CbContentMapper mapper;
+    @Autowired
+    private JedisClient jedisClient;
+
+    // Redis 商品内容 KRY
+    @Value("${INDEX_CONTENT_REDIS_KEY}")
+    private String INDEX_CONTENT_REDIS_KEY;
+
     @Override
     public List<CbContent> getContentList(long contentId) {
+        try {
+            String result = jedisClient.hget(INDEX_CONTENT_REDIS_KEY, contentId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
