2e0856c2f1fa95b7c7c23c444db5fd386e231b3d
StayHungryStayFoolish <bonismo@hotmail.com>
2016-12-08 22:05:54 +0800

    commit

--- rest/src/main/java/com/convenientbuy/rest/controller/ItemController.java
+++ rest/src/main/java/com/convenientbuy/rest/controller/ItemController.java
@@ -1,7 +1,12 @@
 package com.convenientbuy.rest.controller;
 
+import com.convenientbuy.common.pojo.Result;
+import com.convenientbuy.rest.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by bonismo@hotmail.com
@@ -12,4 +17,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/item")
 public class ItemController {
+
+    @Autowired
+    private ItemService service;
+
+    /**
+     * 根据商品 ID 获取基本信息
+     *
+     * @param itemId
+     * @return
+     */
+    @RequestMapping("/info/{itemId}")
+    @ResponseBody
+    public Result getItemBaseInfo(@PathVariable Long itemId) {
+        Result result = service.getItemBaseInfo(itemId);
+        return result;
+    }
+
 }
