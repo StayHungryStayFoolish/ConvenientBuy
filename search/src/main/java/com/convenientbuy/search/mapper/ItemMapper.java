40a641ea368b83f374f370672172d5dc184f4bcd
StayHungryStayFoolish <bonismo@hotmail.com>
2016-12-09 21:50:07 +0800

    commit

---
+++ search/src/main/java/com/convenientbuy/search/mapper/ItemMapper.java
@@ -0,0 +1,14 @@
+package com.convenientbuy.search.mapper;
+
+import com.convenientbuy.search.pojo.Item;
+
+import java.util.List;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午9:49 on 16/12/9.
+ */
+public interface ItemMapper {
+
+    List<Item> getItemList();
+}
