3946bf6a73e3b6603480cb5ae950b64def09a060
StayHungryStayFoolish <bonismo@hotmail.com>
2016-12-09 21:33:45 +0800

    commit

---
+++ search/src/main/java/com/convenientbuy/search/pojo/Item.java
@@ -0,0 +1,91 @@
+package com.convenientbuy.search.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午9:31 on 16/12/9.
+ * <p>
+ * Solr 框架索引库字段
+ */
+public class Item {
+
+    private String id;
+
+    private String title;
+
+    private String sell_point;
+
+    private long price;
+
+    private String image;
+
+    private String category_name;
+
+    private String item_des;
+
+    @JsonIgnore
+    public String[] getImages() {
+        if (image != null) {
+            String[] images = image.split(",");
+            return images;
+        }
+        return null;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSell_point() {
+        return sell_point;
+    }
+
+    public void setSell_point(String sell_point) {
+        this.sell_point = sell_point;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    public String getItem_des() {
+        return item_des;
+    }
+
+    public void setItem_des(String item_des) {
+        this.item_des = item_des;
+    }
+}
