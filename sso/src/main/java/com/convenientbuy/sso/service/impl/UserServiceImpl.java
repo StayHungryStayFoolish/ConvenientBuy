e04a6d1e4c57b427a258d82dfa7102b1b96630dc
StayHungryStayFoolish <bonismo@hotmail.com>
2016-12-17 17:34:19 +0800

    commit

--- sso/src/main/java/com/convenientbuy/sso/service/impl/UserServiceImpl.java
+++ sso/src/main/java/com/convenientbuy/sso/service/impl/UserServiceImpl.java
@@ -3,8 +3,10 @@ package com.convenientbuy.sso.service.impl;
 import com.convenientbuy.common.pojo.Result;
 import com.convenientbuy.mapper.CbUserMapper;
 import com.convenientbuy.pojo.CbUser;
+import com.convenientbuy.sso.dao.JedisClient;
 import com.convenientbuy.sso.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +21,16 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private CbUserMapper mapper;
+
+    @Autowired
+    private JedisClient jedisClient;
+
+    // 用户 session 在 redis 中保存的 key
+    @Value("${REDIS_USER_SESSION_KEY}")
+    private String REDIS_USER_SESSION_KEY;
+    // Session 过期时间 30 分钟
+    @Value("${SSO_SESSION_EXPIRE}")
+    private Integer SSO_SESSION_EXPIRE;
 
     @Override
     public Result checkData(String content, Integer type) {
