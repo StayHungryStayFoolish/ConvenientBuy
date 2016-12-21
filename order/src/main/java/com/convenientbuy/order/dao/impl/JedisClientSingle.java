16c18bc38e5343acad7444752ed9177b14064f24
StayHungryStayFoolish <bonismo@hotmail.com>
2016-12-21 20:17:01 +0800

    commit

---
+++ order/src/main/java/com/convenientbuy/order/dao/impl/JedisClientSingle.java
@@ -0,0 +1,81 @@
+package com.convenientbuy.order.dao.impl;
+
+import com.convenientbuy.order.dao.JedisClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午11:04 on 16/11/29.
+ */
+public class JedisClientSingle implements JedisClient {
+
+    @Autowired
+    private JedisPool jedisPool;
+
+    private Jedis jedis = jedisPool.getResource();
+
+    @Override
+    public String get(String key) {
+        String string = jedis.get(key);
+        jedis.close();
+        return string;
+    }
+
+    @Override
+    public String set(String key, String value) {
+        String string = jedis.set(key, value);
+        jedis.close();
+        return string;
+    }
+
+    @Override
+    public String hget(String hkey, String key) {
+        String string = jedis.hget(hkey, key);
+        jedis.close();
+        return string;
+    }
+
+    @Override
+    public long hset(String hkey, String key, String value) {
+        Long result = jedis.hset(hkey, key, value);
+        jedis.close();
+        return result;
+    }
+
+    @Override
+    public long incr(String key) {
+        Long result = jedis.incr(key);
+        jedis.close();
+        return result;
+    }
+
+    @Override
+    public long expire(String key, int second) {
+        Long result = jedis.expire(key, second);
+        jedis.close();
+        return result;
+    }
+
+    @Override
+    public long ttl(String key) {
+        Long result = jedis.ttl(key);
+        jedis.close();
+        return result;
+    }
+
+    @Override
+    public long del(String key) {
+        Long result = jedis.del(key);
+        jedis.close();
+        return result;
+    }
+
+    @Override
+    public long hdel(String hkey, String key) {
+        Long result = jedis.hdel(hkey, key);
+        jedis.close();
+        return result;
+    }
+}
