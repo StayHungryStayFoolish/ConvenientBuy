d2015d8bc401992f90828f3ca364756660090e08
StayHungryStayFoolish <bonismo@hotmail.com>
2017-10-06 21:43:22 +0800

    commit

--- portal/src/main/java/com/convenientbuy/portal/pojo/SearchResult.java
+++ portal/src/main/java/com/convenientbuy/portal/pojo/SearchResult.java
@@ -10,10 +10,13 @@ public class SearchResult {
 
     //商品列表
     private List<Item> itemList;
+
     //总记录数
     private long recordCount;
+
     //总页数
     private long pageCount;
+
     //当前页
     private long curPage;
 
---
+++ portal/src/main/java/com/convenientbuy/portal/service/ContentService.java
@@ -0,0 +1,14 @@
+package com.convenientbuy.portal.service;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午9:36 on 17/10/6.
+ */
+public interface ContentService {
+
+    /**
+     * 首页获取内容(大广告位)列表
+     * @return
+     */
+    String getContenList();
+}
